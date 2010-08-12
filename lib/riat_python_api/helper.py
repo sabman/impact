467b8371412280a0ae4612f09226add468778622
burqsh <burqsh@6a741046-c63c-430f-8b34-6da8a7d238a7>
2010-08-12 15:58:04 +0000

    added python script to call the model
    git-svn-id: http://www.aifdr.org/svn/riat/source/trunk/riat_rails@116 6a741046-c63c-430f-8b34-6da8a7d238a7

---
+++ lib/riat_python_api/helper.py
@@ -0,0 +1,6 @@
+def parse_named_param(string):
+    """Given a string of the form key=value will return the key value in an array"""
+    k, v = string.split("=")
+    if k == "bbox":
+        val = eval(v)
+    return [k, val]
---
+++ lib/riat_python_api/run_impact_model.py
@@ -0,0 +1,86 @@
+import sys, os, string
+import numpy
+import unittest
+import pycurl
+import StringIO
+import json
+import helper
+
+source_path="../../../AIFDR/aifdr_subversion/riat-source/"
+sys.path.append(source_path)
+sys.path.append("./lib/riat_python_api")
+
+from api import Geoserver, write_coverage_to_ascii
+# from config import webhost, datadir
+
+webhost="www.aifdr.org"
+webhost_local = "localhost"
+datadir="./geodata"
+# Output workspace
+workspace = 'impact'
+
+for arg in sys.argv[1:]:
+    print arg
+    named_param = helper.parse_named_param(arg)
+    if named_param[0] == "bbox":
+        bbox = named_param[1]
+
+print bbox 
+
+# Fatality model parameters (Allen 2010:-)
+a = 0.97429
+b = 11.037
+
+# Locations
+
+if bbox:
+    bounding_box = bbox  # Taken from web application
+else:
+    bounding_box = [95.06, -10.997, 141.001, 5.911]
+
+geoserver = Geoserver('http://%s:8080/geoserver' % webhost,
+                      'admin',
+                      'geoserver')        
+
+geoserver_local = Geoserver('http://%s:8080/geoserver' % webhost_local,
+                    'admin',
+                    'geoserver')    
+
+# Download hazard and exposure data
+layername = 'earthquake_intensity_1hz10pc50'
+print 'Get hazard level:', layername
+hazard_raster = geoserver.get_raster_data(coverage_name=layername,
+                                          bounding_box=bounding_box,
+                                          workspace='hazard',
+                                          verbose=True)            
+            
+#layername = 'landscan_2008'
+layername = 'population_2010'
+print 'Get exposure data:', layername            
+exposure_raster = geoserver.get_raster_data(coverage_name=layername,
+                                            bounding_box=bounding_box,
+                                            workspace='exposure',
+                                            verbose=True)            
+                                                 
+# Calculate impact
+print 'Calculate impact'            
+E = exposure_raster.data
+H = hazard_raster.data
+F = 10**(a*H-b)*E 
+                         
+
+# Store result and upload
+layername = 'earthquake_fatalities_1hz10pc50'          
+                
+output_file = '%s/%s/%s.asc' % (datadir, workspace, layername)
+print 'Store result in:', output_file
+write_coverage_to_ascii(F, output_file, 
+                        xllcorner = bounding_box[0],
+                        yllcorner = bounding_box[1])
+                                                
+# And upload it
+
+print 'Upload to Geoserver:', layername
+geoserver_local.create_workspace(workspace)
+geoserver_local.upload_coverage(filename=output_file, 
+                          workspace=workspace)
