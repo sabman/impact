50a7f6e981a3b584a37d744d35e925d439519c4d
burqsh <burqsh@6a741046-c63c-430f-8b34-6da8a7d238a7>
2010-08-16 02:13:30 +0000

    added the ability to create absolute path to the daemon
    git-svn-id: http://www.aifdr.org/svn/riat/source/trunk/riat_rails@135 6a741046-c63c-430f-8b34-6da8a7d238a7

--- lib/websocket_control.rb
+++ lib/websocket_control.rb
@@ -2,8 +2,10 @@
 
 require "rubygems"
 require "daemons"
+require "FileUtils"
 # check if the file is there
 if File.exists?('./lib/websocket.rb')
+  File.open('/tmp/riat_websocket_root_dir.txt', 'w+') {|f| f.write("#{FileUtils.pwd}") }
   Daemons.run('./lib/websocket.rb')
 else
   raise "File not found: './lib/websocket.rb'"
