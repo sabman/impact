4658aca38224eb17b0405538dad19460f39fb03e
Shoaib Burq <saburq@gmail.com>
2010-08-18 01:22:19 +1000

    added exposure and hazard

--- spec/models/hazard_spec.rb
+++ spec/models/hazard_spec.rb
@@ -1,10 +1,10 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
-
-require "app/models/hazard.rb"
+require 'spec_helper'
 
 describe Hazard do
   it "should return all availalbe layers" do
-    Hazard.layers.should.include({ :name => "l1", :url => "url"})
+    Hazard.layers.should include({
+        "href"=>"http://www.aifdr.org:8080/geoserver/rest/workspaces/hazard/coverages/earthquake_intensity_1hz10pc50.json",
+        "name"=>"earthquake_intensity_1hz10pc50"})
   end
 end
 
--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -7,3 +7,7 @@ require 'machinist/active_record'
 #     title { "Post #{sn}" }
 #     body  { "Lorem ipsum..." }
 #   end
+
+#Exposure.blueprint do
+  # Attributes here
+#end
