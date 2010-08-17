fb72f8cec2776c3f89d7f491fde3d89a53aeb01d
Shoaib Burq <saburq@gmail.com>
2010-08-18 01:30:43 +1000

    added layernames to exposure

--- app/models/exposure.rb
+++ app/models/exposure.rb
@@ -1,7 +1,21 @@
-class Exposure  
+class Exposure
+  @@response = nil
   def self.layers
-    response = HTTParty.get("http://www.aifdr.org:8080/geoserver/rest/workspaces/exposure/coverages.json",
-      { :basic_auth => {:username => "admin", :password => "geoserver"}})
-    response.parsed_response["coverages"]["coverage"]
+    @@response ||= self.get_response
+    @@response.parsed_response["coverages"]["coverage"]
   end  
+  
+  def self.layernames
+    @@response ||= self.get_response
+    layernames = @@response.parsed_response["coverages"]["coverage"].collect{ |h| h["name"]  }
+    layernames
+  end
+  
+  private
+  
+  def self.get_response
+    @@response ||= HTTParty.get("http://www.aifdr.org:8080/geoserver/rest/workspaces/exposure/coverages.json",
+      { :basic_auth => {:username => "admin", :password => "geoserver"}})
+    @@response
+  end
 end
