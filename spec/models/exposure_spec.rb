0accfd4a00cebcf5a5d5a41ce131404716082003
burqsh <burqsh@6a741046-c63c-430f-8b34-6da8a7d238a7>
2010-08-18 03:14:35 +0000

    added layernames to exposure
    git-svn-id: http://www.aifdr.org/svn/riat/source/trunk/riat_rails@152 6a741046-c63c-430f-8b34-6da8a7d238a7

--- spec/models/exposure_spec.rb
+++ spec/models/exposure_spec.rb
@@ -6,5 +6,9 @@ describe Exposure do
       "href"=>"http://www.aifdr.org:8080/geoserver/rest/workspaces/exposure/coverages/population_2010.json", 
       "name"=>"population_2010"})
   end
+  
+  it "should return all layernames as an array" do
+    Exposure.layernames.should include("population_2010")
+  end
 
 end
